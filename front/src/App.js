170ada5cb86249ccd395b42278fafc63feca7543
DarielOWL <darieldesosa@gmail.com>
2022-02-26 00:51:15 -0300

    Se soluciono el problema de añadir todo

--- front/src/App.js
+++ front/src/App.js
@@ -1,147 +1,208 @@
-import React, {
-  useContext,
-  useReducer,
-  useEffect,
-  useRef,
-  useState,
-  createContext,
-} from "react";
+import React, { useContext, useReducer, useEffect, useRef, useState, createContext } from 'react';
 
-//Esto es para establecer una //constante que indica donde esta //nuestro server
 const HOST_API = "http://localhost:8080/api";
-
-//Inicializamos como una array
 const initialState = {
-  list: [],
+  todo: { list: [], item: {} }
 };
+const Store = createContext(initialState)
 
-//Creamos la instancia de la lista
-const Store = createContext(initialState);
 
-//Otro componente que nos permite añadir los todo
 const Form = () => {
-  //Esto hace referencia al objeto cuando se crea
-  //Identifica las propiedades de un componente en //especifico
   const formRef = useRef(null);
-
-  const { dispatch } = useContext(Store);
-  const { state, setState } = useState({});
+  const { dispatch, state: { todo } } = useContext(Store);
+  const item = todo.item;
+  const [state, setState] = useState(item);
 
   const onAdd = (event) => {
     event.preventDefault();
 
-    //Hacemos una request al ingresar ek todo
     const request = {
       name: state.name,
-      description: state.description,
       id: null,
-      isComplete: false,
+      completed: false
     };
 
-    //Mandamos al backend (en formato json)
+
     fetch(HOST_API + "/Createtodo", {
       method: "POST",
       body: JSON.stringify(request),
       headers: {
-        "Content-Type": "application/json",
-      },
+        'Content-Type': 'application/json'
+      }
     })
-      .then((response) => response.json())
+      .then(response => response.json())
       .then((todo) => {
         dispatch({ type: "add-item", item: todo });
         setState({ name: "" });
         formRef.current.reset();
       });
-  };
+  }
 
-  //Esto sirve para input de datos del todo
-  return (
-    <form ref={formRef}>
-      <input
-        type="text"
-        name="name"
-        onChange={(event) => {
-          setState({ ...state, name: event.target.value });
-        }}
-      ></input>
-      <button onClick={onAdd}>Agregar</button>
-    </form>
-  );
-};
+  const onEdit = (event) => {
+    event.preventDefault();
 
-//Listar los todos
+    const request = {
+      name: state.name,
+      id: item.id,
+      isCompleted: item.isCompleted
+    };
+
+
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
+        setState({ name: "" });
+        formRef.current.reset();
+      });
+  }
+
+  return <form ref={formRef}>
+    <input
+      type="text"
+      name="name"
+      placeholder="¿Qué piensas hacer hoy?"
+      defaultValue={item.name}
+      onChange={(event) => {
+        setState({ ...state, name: event.target.value })
+      }}  ></input>
+    {item.id && <button onClick={onEdit}>Actualizar</button>}
+    {!item.id && <button onClick={onAdd}>Crear</button>}
+  </form>
+}
+
+
 const List = () => {
-  //Esto es un contexto (el contexto Store guarda el estado del objeto)
-  const { dispatch, state } = useContext(Store);
+  const { dispatch, state: { todo } } = useContext(Store);
+  const currentList = todo.list;
 
-  //El useEffect nos permite trabajar en
-  //background (hace que no se tranque cuando se hace una peticion al backend)
   useEffect(() => {
     fetch(HOST_API + "/todos")
-      .then((response) => response.json())
+      .then(response => response.json())
       .then((list) => {
-        dispatch({ type: "update-list", list });
+        dispatch({ type: "update-list", list })
+      })
+  }, [dispatch]);
+
+
+  const onDelete = (id) => {
+    fetch(HOST_API + "/" + id + "/todo", {
+      method: "DELETE"
+    }).then((list) => {
+      dispatch({ type: "delete-item", id })
+    })
+  };
+
+  const onEdit = (todo) => {
+    dispatch({ type: "edit-item", item: todo })
+  };
+
+  const onChange = (event, todo) => {
+    const request = {
+      name: todo.name,
+      id: todo.id,
+      completed: event.target.checked
+    };
+    fetch(HOST_API + "/todo", {
+      method: "PUT",
+      body: JSON.stringify(request),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then((todo) => {
+        dispatch({ type: "update-item", item: todo });
       });
-  }, [state.list.length, dispatch]);
+  };
 
-  return (
-    <div>
-      <table>
-        <thead>
-          <tr>
-            <td>ID</td>
-            <td>Nombre</td>
-            <td>¿Esta Completado?</td>
+  const decorationDone = {
+    textDecoration: 'line-through'
+  };
+  return <div>
+    <table >
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Tarea</td>
+          <td>¿Completado?</td>
+        </tr>
+      </thead>
+      <tbody>
+        {currentList.map((todo) => {
+          return <tr key={todo.id} style={todo.completed ? decorationDone : {}}>
+            <td>{todo.id}</td>
+            <td>{todo.name}</td>
+            <td><input type="checkbox" defaultChecked={todo.completed} onChange={(event) => onChange(event, todo)}></input></td>
+            <td><button onClick={() => onDelete(todo.id)}>Eliminar</button></td>
+            <td><button onClick={() => onEdit(todo)}>Editar</button></td>
           </tr>
-        </thead>
-        <tbody>
-          {state.list.map((todo) => {
-            return (
-              <tr key={todo.id}>
-                <td>{todo.id}</td>
-                <td>{todo.name}</td>
-                <td>{todo.isCompleted}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
-  );
-};
+        })}
+      </tbody>
+    </table>
+  </div>
+}
 
-//Hacer el reduce
+
+
 function reducer(state, action) {
   switch (action.type) {
-    case "update-list":
-      return { ...state, list: action.list };
-    case "add-item":
-      const newList = state.list;
-      newList.push(action.item);
-      return { ...state, list: newList };
+    case 'update-item':
+      const todoUpItem = state.todo;
+      const listUpdateEdit = todoUpItem.list.map((item) => {
+        if (item.id === action.item.id) {
+          return action.item;
+        }
+        return item;
+      });
+      todoUpItem.list = listUpdateEdit;
+      todoUpItem.item = {};
+      return { ...state, todo: todoUpItem }
+    case 'delete-item':
+      const todoUpDelete = state.todo;
+      const listUpdate = todoUpDelete.list.filter((item) => {
+        return item.id !== action.id;
+      });
+      todoUpDelete.list = listUpdate;
+      return { ...state, todo: todoUpDelete }
+    case 'update-list':
+      const todoUpList = state.todo;
+      todoUpList.list = action.list;
+      return { ...state, todo: todoUpList }
+    case 'edit-item':
+      const todoUpEdit = state.todo;
+      todoUpEdit.item = action.item;
+      return { ...state, todo: todoUpEdit }
+    case 'add-item':
+      const todoUp = state.todo.list;
+      todoUp.push(action.item);
+      return { ...state, todo: {list: todoUp, item: {}} }
     default:
       return state;
   }
 }
 
-//El Provider nos permite conectar diferentes componentes
 const StoreProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  return (
-    <Store.Provider value={{ state, dispatch }}>{children}</Store.Provider>
-  );
-};
+  return <Store.Provider value={{ state, dispatch }}>
+    {children}
+  </Store.Provider>
 
-//Vista del navegador (Esto vendria ser como la funcion Main)
+}
+
 function App() {
-  return (
-    <StoreProvider>
-      <h3>To-Do List - By Dáriel</h3>
-      <Form />
-      <List />
-    </StoreProvider>
-  );
+  return <StoreProvider>
+    <h3>To-Do List - By Dáriel</h3>
+    <Form />
+    <List />
+  </StoreProvider>
 }
 
 export default App;
